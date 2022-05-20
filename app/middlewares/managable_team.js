f21cf7e080d0c118d83a61978b2641147be2851a
c_yamada <c_yamada@uluru.jp>
2022-05-20 12:09:11 +0900

    Delete-console

--- app/middlewares/managable_team.js
+++ app/middlewares/managable_team.js
@@ -1,6 +1,5 @@
 module.exports = async function forceManage(req, res, next) {
   const member = await req.user.getOwnMembers({ where: { teamId: req.params.team, role: 1 } });
-  console.log(member);
   if (member.length > 0) {
     return next();
   }
